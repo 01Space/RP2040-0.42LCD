ea530a8ce88650f380a7739ac2949dd4db896bfb
01Space <94150584+01Space@users.noreply.github.com>
2022-04-16 09:32:33 +0800

    Add files via upload

---
+++ Arduino/RP2040-0.42LCD-TEST/RP2040-0.42LCD-TEST.ino
@@ -0,0 +1,104 @@
+/*
+  Blink without Delay
+
+  Turns on and off a light emitting diode (LED) connected to a digital pin,
+  without using the delay() function. This means that other code can run at the
+  same time without being interrupted by the LED code.
+
+  The circuit:
+  - Use the onboard LED.
+  - Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA
+    and ZERO it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN
+    is set to the correct LED pin independent of which board is used.
+    If you want to know what pin the on-board LED is connected to on your
+    Arduino model, check the Technical Specs of your board at:
+    https://www.arduino.cc/en/Main/Products
+
+  created 2005
+  by David A. Mellis
+  modified 8 Feb 2010
+  by Paul Stoffregen
+  modified 11 Nov 2013
+  by Scott Fitzgerald
+  modified 9 Jan 2017
+  by Arturo Guadalupi
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/BlinkWithoutDelay
+*/
+
+// constants won't change. Used here to set a pin number:
+const int ledPin1 =  29;// the number of the LED pin
+const int ledPin2 =  28;// the number of the LED pin
+const int ledPin3 =  27;// the number of the LED pin
+const int ledPin4 =  26;// the number of the LED pin
+const int ledPin5 =  25;// the number of the LED pin
+const int ledPin6 =  24;// the number of the LED pin
+const int ledPin7 =  20;// the number of the LED pin
+const int ledPin8 =  5;// the number of the LED pin
+const int ledPin9=  6;// the number of the LED pin
+const int ledPin10 = 4;// the number of the LED pin
+const int ledPin11 = 3;// the number of the LED pin
+
+// Variables will change:
+int ledState = LOW;             // ledState used to set the LED
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change:
+const long interval = 1000;           // interval at which to blink (milliseconds)
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
+  pinMode(ledPin8, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
+  pinMode(ledPin11, OUTPUT);
+  
+  
+}
+
+void loop() {
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin1, ledState);
+    digitalWrite(ledPin2, ledState);
+    digitalWrite(ledPin3, ledState);
+    digitalWrite(ledPin4, ledState);
+    digitalWrite(ledPin5, ledState);
+    digitalWrite(ledPin6, ledState);
+    digitalWrite(ledPin7, ledState);
+    digitalWrite(ledPin8, ledState);
+    digitalWrite(ledPin9, ledState);
+    digitalWrite(ledPin10, ledState);
+    digitalWrite(ledPin11, ledState);
+    
+  }
+}
